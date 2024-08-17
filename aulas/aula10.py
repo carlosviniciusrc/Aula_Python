2aa2f0e2915cbd14385242378eb9fdf2fa4b86dd
Carlos Vinicius <carlosviniciusrcdev@gmail.com>
2024-08-17 10:32:54 -0300

    att

---
+++ aulas/aula10.py
@@ -0,0 +1,13 @@
+# Concatenação (+) e repetição (*) com operadores aritméticos
+
+concatenacao = 'A' + 'B' + 'C' # Em caso de strings o sinal de + vai fazer a junção
+concatenacao2 = 'Vinixs' + str(0) + str(4)
+
+print(concatenacao)
+print(concatenacao2)
+
+repeticao1 = 'V' * 6 # Nesse caso a STtring vai ser multiplicada
+repeticao2 = 'Vini' * 4 
+
+print(repeticao1)
+print(repeticao2)
