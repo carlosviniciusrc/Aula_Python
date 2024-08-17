6602864318842156480f9f7002b1d41077322728
Carlos Vinicius <carlosviniciusrcdev@gmail.com>
2024-08-17 10:14:45 -0300

    att

---
+++ aulas/aula9.py
@@ -0,0 +1,27 @@
+# Introdução aos operadores aritmeticos(matemática)
+
+adicao = 10 + 10
+print('Adição', adicao, sep='=')
+
+subtracao = 10 - 5
+print('Subtração', subtracao, sep='=')
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao, sep='=')
+
+divisao = 10 / 2 # Dependendo da divisão vai retornar como float
+print('Divisão', divisao, sep='=')
+
+divisao_inteira = 10 // 2 # Ele vai cortar casas decimais
+print('Divisão inteira', divisao_inteira, sep='=')
+
+exponenciacao = 2 ** 10 # Elevação
+print('Exponeciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisao
+print('Módulo', modulo)
+
+# Exemplo para saber se o numero é divisivel, ou par e impar
+
+print(10 % 2 == 0)
+print(15 % 2 == 0)
